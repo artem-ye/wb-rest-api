ada9bc1042266c9ad144402352d8c0381531c1d4
ay <shlemafona@gmail.com>
2017-09-29 10:36:42 +0300

    index.hmtl edited

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
-
-
+<!--
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-
+-->
 
 <?php
 
-
     require_once './include/API.php';
-
 
     $api = new API;
     $api->processApi();
 
-
 ?>
 
+<!--
 </body>
 </html>
+-->
 
 
 
